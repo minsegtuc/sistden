94e255f5a1f4f91211d5369f06f1244875ea81e7
luciano-acs <luciano.acosta@alu.frt.utn.edu.ar>
2025-09-05 20:20:43 -0300

    prueba 19

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { registerSW } from 'virtual:pwa-register'
+
+const updateSW = registerSW({
+    onNeedRefresh() {
+        // Muestra una notificación al usuario o un botón para refrescar
+        if (confirm('Hay una nueva versión de la app. ¿Deseas recargar para actualizar?')) {
+            updateSW(true); // Fuerza la actualización del SW y recarga la página
+        }
+    },
+    onOfflineReady() {
+        console.log('App lista para trabajar offline');
+    },
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,56 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
-// https://vitejs.dev/config/ 
+
 export default defineConfig({
-  plugins: [react(), VitePWA({ base: '/', registerType: 'autoUpdate', manifest: { "name": "Sistema de control de gestión", "short_name": "SCG", "start_url": "/", "display": "standalone", "background_color": "#000000", "theme_color": "#005CA2", "icons": [{ "src": "/img_logo.png", "sizes": "192x192", "type": "image/png" }, { "src": "/img_logo.png", "sizes": "512x512", "type": "image/png" }] }, workbox: { clientsClaim: true, skipWaiting: true, runtimeCaching: [{ urlPattern: /\/\/.*\.(js|css|html|png|jpg|jpeg|svg|ico)$/, handler: 'StaleWhileRevalidate', options: { cacheName: 'static-assets', expiration: { maxEntries: 50, maxAgeSeconds: 60 * 60 * 24, }, }, }, { urlPattern: /\/\/$/, handler: 'NetworkFirst', options: { cacheName: 'html-pages', expiration: { maxEntries: 5, maxAgeSeconds: 60 * 60 * 24 * 365, }, }, },] } })], base: '/',
-  // server: { // host: '0.0.0.0', // } 
+  plugins: [react(), VitePWA({
+    base: '/',
+    registerType: 'autoUpdate',
+    manifest: {
+      "name": "Sistema de control de gestión",
+      "short_name": "SCG",
+      "start_url": "/",
+      "display": "standalone",
+      "background_color": "#000000",
+      "theme_color": "#005CA2",
+      "icons": [{
+        "src": "/img_logo.png",
+        "sizes": "192x192",
+        "type": "image/png"
+      },
+      {
+        "src": "/img_logo.png",
+        "sizes": "512x512",
+        "type": "image/png"
+      }]
+    },
+    workbox: {
+      clientsClaim: true,
+      skipWaiting: true,
+      runtimeCaching: [{
+        urlPattern: /\/\/.*\.(js|css|html|png|jpg|jpeg|svg|ico)$/,
+        handler: 'StaleWhileRevalidate',
+        options: {
+          cacheName: 'static-assets',
+          expiration: {
+            maxEntries: 50,
+            maxAgeSeconds: 60 * 60 * 24,
+          },
+        },
+      },
+      {
+        urlPattern: /\/\/$/,
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'html-pages',
+          expiration: {
+            maxEntries: 5,
+            maxAgeSeconds: 60 * 60 * 24 * 365,
+          },
+        },
+      },
+      ]
+    }
+  })], 
+  base: '/'
 })
