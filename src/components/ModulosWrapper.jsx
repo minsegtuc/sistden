bb12903b3f1480b008ecf2b6ace162b3138ace25
luciano-acs <luciano.acosta@alu.frt.utn.edu.ar>
2025-09-11 10:35:33 -0300

    cambio modulos roles

--- src/components/ModulosWrapper.jsx
+++ src/components/ModulosWrapper.jsx
@@ -6,12 +6,12 @@ const ModulosWrapper = () => {
     const { user, serverlocal } = useContext(ContextConfig);
 
     const modulos = [
-        { nombre: "admin", enlace: `${serverlocal}/admin/`, roles: ['SISADMIN'] },
-        { nombre: "datos", enlace: `${serverlocal}/datos/`, roles: ['SISADMIN', 'MINISTERIO'] },
-        { nombre: "ingreso", enlace: `${serverlocal}/ingreso/estadisticas`, roles: ['SISADMIN', 'MINISTERIO', 'VINGRESO'] },
-        { nombre: "mapa", enlace: `${serverlocal}/mapa-operativo/`, roles: ['SISADMIN', 'MINISTERIO', 'VMAPA', 'CAMARAS'] },
-        { nombre: "denuncias", enlace: `${serverlocal}/sgd/`, roles: ['SISADMIN', 'MINISTERIO'] },
-    ];
+        { nombre: "admin", img: `/logo_admin_v2.png`, imgHover: '/logo_admin_v2_blanco.png', enlace: `${serverlocal}/admin/`, roles: ['SISADMIN'] },
+        { nombre: "datos", img: `/carga_datos_logo.png`, imgHover: '/carga_datos_logo_blanco.png', enlace: `${serverlocal}/datos/`, roles: ['SISADMIN', 'ESTADISTICAS'] },
+        { nombre: "ingreso", img: `/logo_ingreso_negro.png`, imgHover: '/logo_ingreso_blanco.png', enlace: `${serverlocal}/ingreso/estadisticas`, roles: ['SISADMIN', 'DENING','VINGRESO','IESTADISTICAS'] },
+        { nombre: "mapa", img: `/logo_mapa_operativo_negro.svg`, imgHover: '/logo_mapa_operativo_blanco.svg', enlace: `${serverlocal}/mapa-operativo/`, roles: ['SISADMIN', 'ESTADISTICAS', 'VMAPA', 'CAMARAS','DENING','DENUNCIAS'] },
+        { nombre: "denuncias", img: `/sgd_logo_negro.png`, imgHover: '/sgd_logo_blanco.png', enlace: `${serverlocal}/sgd/`, roles: ['SISADMIN','DENING','ESTADISTICAS','DENUNCIAS'] },
+    ]
 
     const modulosPermitidos = modulos.filter(m => m.roles.includes(user.rol));
 
--- src/pages/Modulos.jsx
+++ src/pages/Modulos.jsx
@@ -9,7 +9,7 @@ const Modulos = () => {
     const modulos = [
         { nombre: "admin", img: `/logo_admin_v2.png`, imgHover: '/logo_admin_v2_blanco.png', enlace: `${serverlocal}/admin/`, roles: ['SISADMIN'] },
         { nombre: "datos", img: `/carga_datos_logo.png`, imgHover: '/carga_datos_logo_blanco.png', enlace: `${serverlocal}/datos/`, roles: ['SISADMIN', 'ESTADISTICAS'] },
-        { nombre: "ingreso", img: `/logo_ingreso_negro.png`, imgHover: '/logo_ingreso_blanco.png', enlace: `${serverlocal}/ingreso/estadisticas`, roles: ['SISADMIN', 'DENING','VINGRESO'] },
+        { nombre: "ingreso", img: `/logo_ingreso_negro.png`, imgHover: '/logo_ingreso_blanco.png', enlace: `${serverlocal}/ingreso/estadisticas`, roles: ['SISADMIN', 'DENING','VINGRESO','IESTADISTICAS'] },
         { nombre: "mapa", img: `/logo_mapa_operativo_negro.svg`, imgHover: '/logo_mapa_operativo_blanco.svg', enlace: `${serverlocal}/mapa-operativo/`, roles: ['SISADMIN', 'ESTADISTICAS', 'VMAPA', 'CAMARAS','DENING','DENUNCIAS'] },
         { nombre: "denuncias", img: `/sgd_logo_negro.png`, imgHover: '/sgd_logo_blanco.png', enlace: `${serverlocal}/sgd/`, roles: ['SISADMIN','DENING','ESTADISTICAS','DENUNCIAS'] },
     ]
