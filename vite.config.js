e7bebf62a1cb9ab7df56f5bae509fe7a127e2f50
luciano-acs <luciano.acosta@alu.frt.utn.edu.ar>
2025-09-15 20:24:00 -0300

    prueba swa

--- vite.config.js
+++ vite.config.js
@@ -30,6 +30,7 @@ export default defineConfig({
     workbox: {
       clientsClaim: true,
       skipWaiting: true,
+      globIgnores: ['/ingreso/**'],
       runtimeCaching: [
         {
           urlPattern: ({ url }) => url.pathname.startsWith('/ingreso/'),
