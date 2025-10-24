cd338a53adcd26425c3369a5c7eca07b33197ff1
luciano-acs <luciano.acosta@alu.frt.utn.edu.ar>
2025-10-24 11:10:11 -0300

    nuevo modulo

--- src/pages/Modulos.jsx
+++ src/pages/Modulos.jsx
@@ -10,7 +10,7 @@ const Modulos = () => {
         { nombre: "admin", img: `/logo_admin_v2.png`, imgHover: '/logo_admin_v2_blanco.png', enlace: `${serverlocal}/admin/`, roles: ['SISADMIN'] },
         { nombre: "datos", img: `/carga_datos_logo.png`, imgHover: '/carga_datos_logo_blanco.png', enlace: `${serverlocal}/mapa-operativo/homicidios`, roles: ['SISADMIN', 'ESTADISTICAS','MINISTERIO'] },
         { nombre: "ingreso", img: `/logo_ingreso_negro.png`, imgHover: '/logo_ingreso_blanco.png', enlace: `${serverlocal}/ingreso/estadisticas`, roles: ['SISADMIN', 'DENING','VINGRESO','IESTADISTICAS'] },
-        { nombre: "mapa", img: `/logo_mapa_operativo_negro.svg`, imgHover: '/logo_mapa_operativo_blanco.svg', enlace: `${serverlocal}/mapa-operativo/`, roles: ['SISADMIN', 'ESTADISTICAS', 'VMAPA', 'CAMARAS','DENING','DENUNCIAS','MINISTERIO'] },
+        { nombre: "mapa", img: `/logo_mapa_operativo_negro.svg`, imgHover: '/logo_mapa_operativo_blanco.svg', enlace: `${serverlocal}/mapa-operativo/`, roles: ['SISADMIN', 'ESTADISTICAS', 'VMAPA', 'CAMARAS','DENING','DENUNCIAS','MINISTERIO','POLICIA'] },
         { nombre: "denuncias", img: `/sgd_logo_negro.png`, imgHover: '/sgd_logo_blanco.png', enlace: `${serverlocal}/sgd/`, roles: ['SISADMIN','DENING','ESTADISTICAS','DENUNCIAS'] },
     ]
 
